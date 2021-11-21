537fa8896d6cd782799619a699d2f115b6796410
Raviraj Dulange <redhood.dumb@gmail.com>
2021-11-21 23:33:18 +0530

    Design the modal

--- src/components/Home/Header/ComingSoon.jsx
+++ src/components/Home/Header/ComingSoon.jsx
@@ -3,14 +3,14 @@ import "./ComingSoon.css";
 import imgSrc from "../../../assets/commingSoon.png";
 export default function ComingSoon(props) {
   return (
-    <div className="popUpBox">
+    <div className="popUpBox" onClick={props.onClick}>
       <div style={{ margin: "16px auto 50px" }}>
         <h1>{props.title}</h1>
       </div>
       <img src={imgSrc} alt="Coming Soon" className="popUpImg" />
-      <div style={{ margin: "40px 10px 50px" }}>
+      <div style={{ margin: "40px 10% 50px" }}>
         <h4 style={{ lineHeight: "30px" }}>
-          We are currently working on this feature and will launch soon
+          We are currently working on this feature and will launch soon!
         </h4>
       </div>
     </div>
